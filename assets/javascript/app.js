bd21df7396f97f2f426a7e747f4cde775556943c
4751 <60683342+4751@users.noreply.github.com>
2023-01-08 11:45:23 +0530

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ class _app {
   backgroundToggler = false;
   shouldIgnoreVideo = false;
   effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
+  brandDescription = ['OG#2624', 'trashtalkers', 'hack vs hack team', 'notoriou$', 'winning since 2009', 'You are listening to: Tazdied - Dead to me'];
 
   titleChanger = (text, delay) => {
     if (!text) return;
