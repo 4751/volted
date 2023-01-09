51e0df38fb3c600dc7be556a4347bd622a70b564
4751 <60683342+4751@users.noreply.github.com>
2023-01-09 10:23:43 +0530

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -70,7 +70,7 @@ $(document).ready(() => {
   }
 
   if (mobileAndTabletCheck()) {
-    $('#background').replaceWith('<div id="background" style="background-image: url(assets/images/mobile-background.jpg);"></div>');
+    $('#background').replaceWith('<div id="background" style="background-image: url(assets/images/mobile-background.png);"></div>');
 
     app.shouldIgnoreVideo = true;
   }
